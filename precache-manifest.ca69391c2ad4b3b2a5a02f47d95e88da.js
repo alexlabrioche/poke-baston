a489df5e72065670a5db128b00fd85d9893ffd67
Alex Baker <alexbakerdeveloper@gmail.com>
2020-02-05 11:10:48 +0100

    Updates

---
+++ precache-manifest.ca69391c2ad4b3b2a5a02f47d95e88da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0324e103abe12ab08df40fd2e7023c",
+    "url": "/alexlabrioche/poke-baston/index.html"
+  },
+  {
+    "revision": "75ab9596da0e8231b713",
+    "url": "/alexlabrioche/poke-baston/static/css/main.7ed9fd3c.chunk.css"
+  },
+  {
+    "revision": "84f12d03a007d61f35c3",
+    "url": "/alexlabrioche/poke-baston/static/js/2.c088d2e6.chunk.js"
+  },
+  {
+    "revision": "39bb7413043604e2d1c80806d480f8cd",
+    "url": "/alexlabrioche/poke-baston/static/js/2.c088d2e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ab9596da0e8231b713",
+    "url": "/alexlabrioche/poke-baston/static/js/main.032899c9.chunk.js"
+  },
+  {
+    "revision": "68e2752435d714fc1ab7",
+    "url": "/alexlabrioche/poke-baston/static/js/runtime-main.4540469d.js"
+  }
+]);
